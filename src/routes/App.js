bead44ca64b9253dd9d196bea5f380b39745afee
Abigailal <abigail_al@outlook.com>
2022-08-05 11:55:59 -0500

    Deploy

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-//import { Layout } from '../containers/Layout';
-import { Home } from '../pages/Home/Home';
-import { NotFound } from '../pages/NotFound/NotFound';
 import { TaskMenu } from '../pages/TaskMenu/TaskMenu';
 import {TodoContext} from '../context/TodoContext';
 import { useTodos } from '../hooks/useTodos';
@@ -11,13 +7,7 @@ function App() {
 	const initialState = useTodos();
   	return (
 		<TodoContext.Provider value = {initialState}>
-    		<BrowserRouter basename="/ToDo-App">
-				<Routes>
-					<Route exact path="/" element={<Home/>} />
-					<Route exact path="/Task-Menu" element={<TaskMenu/>} />
-          			<Route path="*" element={<NotFound/>} />
-				</Routes>
-			</BrowserRouter>
+			<TaskMenu/>
 		</TodoContext.Provider>
   	);
 }
